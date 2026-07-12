4f69f13c20e6a689ed892c68e66f80b1b16a1044
SHIVAM PRASAD <shivamkr.verma82@gmail.com>
2026-07-12 09:12:34 +0530

    Time: 30 ms (71.06%), Space: 75.3 MB (63.77%) - LeetHub

---
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] arrayRankTransform(int[] arr) {
+        // create clone of array to sort the array due to initializing rank for min number (1)
+        int[] sorted = arr.clone();
+        Arrays.sort(sorted); 
+        int rank = 1;
+        // map stores the value and it's rank
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < arr.length; i++){
+            // similar values share the same rank
+            // different values have different rank
+            if(!map.containsKey(sorted[i])){
+                map.put(sorted[i], rank++);
+            }
+        }
+        // ans stores the rank of each element
+        int ans[] = new int[arr.length];
+
+        for(int i = 0; i < ans.length; i++){
+            ans[i] = map.get(arr[i]);  // give the rank on the basis of element
+        }
+        // return rank transformed array
+        return ans;
+    }
+}
