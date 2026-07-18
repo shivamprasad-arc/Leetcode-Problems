3b116d65b5262bdc0ca62d7a4659f92de992ab1e
SHIVAM PRASAD <shivamkr.verma82@gmail.com>
2026-07-18 12:10:50 +0530

    Time: 0 ms (100%), Space: 45 MB (58.42%) - LeetHub

--- 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.java
+++ 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.java
@@ -1,17 +1,26 @@
 class Solution {
-    // Time complexity = O(N logN)
-    // Space Complexity = O(N)
+    // Time Complexity: O(N)
+    // Space Complexity: O(1)
     public int findGCD(int[] nums) {
-        // sort the array to find the smallest and largest number
-        Arrays.sort(nums);
-        int smallest = nums[0];
-        int largest = nums[nums.length - 1];
-        // find the GCD of smallest and largest number of the array
+        int smallest = Integer.MAX_VALUE;
+        int largest = Integer.MIN_VALUE;
+        // find the smallest and largest element of the array
+        for(int i = 0; i < nums.length; i++){
+            if(smallest >= nums[i]){
+                smallest = nums[i];
+            }
+            if(largest <= nums[i]){
+                largest = nums[i];
+            }
+        }
+
+        // Find the GCD of smallest and largest element of the array
         while(smallest != 0){
             int temp = smallest;
             smallest = largest % smallest;
             largest = temp;
         }
+        // return the largest element that is GCD of smallest and largest element
         return largest;
     }
 }
