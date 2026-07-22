f2e62e9fe6474f9d61547b96b0f88e6b2e38624d
SHIVAM PRASAD <shivamkr.verma82@gmail.com>
2026-07-22 19:06:14 +0530

    Time: 3 ms (35.79%), Space: 43.2 MB (6.45%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
@@ -6,9 +6,14 @@ class Solution {
         HashSet<Character> set = new HashSet<>();
         for(int i = 0; i < sentence.length(); i++){
             set.add(sentence.charAt(i));
+
+            // if the size of set is greater or equal to 26 then the sentence is panagram i.e., 
+            // the sentence contains all letters of the english alphabets
+            if(set.size() == 26){
+                return true;
+            }
         }
-        // if the size of set is greater or equal to 26 then the sentence is panagram i.e., 
-        // the sentence contains all letters of the english alphabets
-        return set.size() >= 26 ? true : false;
+        // if the size of set is less than 26 then return false
+        return false;
     }
 }
