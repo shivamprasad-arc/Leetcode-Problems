1a4b3346ccbefcf3d09a00e0f7afd1320d68466d
SHIVAM PRASAD <shivamkr.verma82@gmail.com>
2026-07-23 09:00:07 +0530

    Time: 2 ms (98.04%), Space: 43.6 MB (69.09%) - LeetHub

---
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
@@ -0,0 +1,24 @@
+class Solution {
+    // Time complexity: O(N)
+    // Space Complexity: O(N+N) = O(N)
+    public boolean uniqueOccurrences(int[] arr) {
+        // map is used to store the number and their occurences
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < arr.length; i++){
+            map.put(arr[i], map.getOrDefault(arr[i], 0)+1);
+        }
+        // set stores the unique occurences of the map 
+        // if the occurences is same then ignore it
+        HashSet<Integer> set = new HashSet<>();
+        for(Integer val: map.keySet())
+        {
+            set.add(map.get(val));
+        }
+        // if the size of both map and set is equal then each element of array has unique occurences
+        // else if the size of both map and set is different then the each element of array hasn't unique occurence
+        if(map.size() == set.size()){
+            return true;
+        }
+        return false;
+    }
+}
