5555236586f13ae0a2967644a5a6c60c7db56399
SHIVAM PRASAD <shivamkr.verma82@gmail.com>
2026-07-28 07:18:09 +0530

    Time: 33 ms (43.78%), Space: 48 MB (88.35%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.java
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.java
@@ -9,13 +9,7 @@ class Solution {
         int mid = n / 2;
 
         // Sort the half portion of the array
-        if(n % 2 != 0){
-            // in even length sort the characters from 0 to mid - 1
-            Arrays.sort(arr, 0, mid);
-        }else{
-            // in odd length sort the characters from 0 to mid
-            Arrays.sort(arr, 0,  mid);
-        }
+        Arrays.sort(arr, 0, mid);
 
         // copy the characters of sorted left half into to right half
         int left = 0;
