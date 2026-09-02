2feb9b066b5f2690c163dd769e6530136561659c
SHIVAM PRASAD <shivamkr.verma82@gmail.com>
2026-09-02 17:55:28 +0530

    Time: 0 ms (100%), Space: 45 MB (73.43%) - LeetHub

---
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
@@ -0,0 +1,7 @@
+class Solution {
+    // this function always returns true because every array can be converted into a uniform parity array.
+    public boolean uniformArray(int[] nums1) {
+        return true;
+        
+    }
+}
